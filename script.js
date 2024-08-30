c9dd07ee788181a4ce5484ae0a998645a0c021cf
Fuck Linux <89513519+fucklinux@users.noreply.github.com>
2024-08-30 11:59:37 +0800

    v1.0.3 版本更新
    -排序时不区分大小写
    -进行新建、重命名、删除操作时检查是否成功
    -进行新建、重命名操作时检测非法字符
    -支持为当前目录添加书签
    -完善文件类型图标库
    -优化一些 UI 细节

--- script.js
+++ script.js
@@ -5,24 +5,26 @@ window.addEventListener("load", function () {
     // 禁用加载效果和下拉刷新
     window.webapp.circle(false);
     window.webapp.renew(false);
-    // 检测是否有存储权限
+    // 检测有无存储权限
     if (window.webapp.bestow()) {
         // 载入设置
         loadSettings();
         // 打开启动目录
         if (settings.bootDirectory == "上次打开的目录") {
             loadFiles(localStorage.getItem("lastPath"));
+            document.querySelector("#path").innerText = localStorage.getItem("lastPath");
         } else {
             loadFiles(settings.bootDirectory);
+            document.querySelector("#path").innerText = settings.bootDirectory;
         }
         // 获取书签
-        loadBookmark();
+        loadBookmarks();
         // 响应式显示抽屉栏
         setTimeout(function () {
             document.documentElement.clientWidth >= 840 ? (document.querySelector("#drawer").open = true) : undefined;
         }, 300);
     } else {
-        document.querySelector("#setupPermission").open = true;
+        document.querySelector("#setupPermissionDialog").open = true;
     }
     // 申请存储权限回调
     window.webapp.behold("navigation.reload();");
@@ -49,18 +51,25 @@ window.addEventListener("load", function () {
         document.querySelector("#pathInput").focus();
     });
     document.querySelector("#multipleSelectDialog").addEventListener("open", function () {
+        // 获取已显示的文件列表
+        var fileListItems = document.querySelectorAll(".file-list-item");
+        // 初始化文件列表的 HTML 代码
         var html = "";
-        document.querySelectorAll(".file-list-item").forEach(function (item) {
+        fileListItems.forEach(function (item) {
             if (item.style.display != "none") {
                 html += `<mdui-list-item headline="` + item.headline + `" description="` + (item.description == undefined ? "" : item.description) + `" icon="` + item.icon + `" class="multiple-select-list-item">
     <mdui-checkbox slot="end-icon" name="multipleSelectCheckbox"></mdui-checkbox>    
 </mdui-list-item>`;
             }
         });
+        if (html == "") {
+            html += `<div style="text-align: center;margin: 16px 0;">没有项目</div>`;
+        }
+        // 填入文件列表的 HTML 代码
         document.querySelector("#multipleSelectList").innerHTML = html;
     });
     // 自动刷新文件列表
-    document.querySelector("#settings").addEventListener("closed", function () {
+    document.querySelector("#settingsDialog").addEventListener("closed", function () {
         refreshFileList();
     });
     // 回车键操作
@@ -145,10 +154,7 @@ function changeSettings(key, value) {
             settings.sortBy = value;
             localStorage.setItem("settings", JSON.stringify(settings));
             loadSettings();
-            // 避免菜单关闭动画卡顿
-            setTimeout(function () {
-                refreshFileList();
-            }, 250);
+            refreshFileList();
             break;
         default:
             break;
@@ -187,7 +193,7 @@ function onTouchEnd() {
 // 载入文件列表
 function loadFiles(path) {
     try {
-        // 分割出文件列表
+        // 获取并分割出文件列表
         var files = window.webapp.listfile(path).split("\\");
         // 初始化文件列表的 HTML 代码
         var html = "";
@@ -199,11 +205,15 @@ function loadFiles(path) {
                 break;
             case "name":
                 // 名称
-                files = files.sort();
+                files = files.sort(function (a, b) {
+                    return a.toLowerCase().localeCompare(b.toLowerCase());
+                });
                 break;
             case "name_reverse":
                 // 名称-降序
-                files = files.sort().reverse();
+                files = files.sort(function (a, b) {
+                    return a.toLowerCase().localeCompare(b.toLowerCase());
+                }).reverse();
                 break;
             default:
                 break;
@@ -229,10 +239,10 @@ function loadFiles(path) {
             var pathCopy = path.slice(0, path.length - 2);
             html += `<mdui-list-item rounded headline=".." icon="drive_folder_upload" onclick="openItem('` + (pathCopy.slice(0, pathCopy.lastIndexOf("/") + 1)) + `');"></mdui-list-item>`;
         }
-        // 判断是否为空文件夹
-        if (files != "/") {
+        // 判断当前目录是否为空
+        if (files != "/" && files != ",") {
             files.forEach(function (item) {
-                // 判断是否为隐藏文件（夹）
+                // 判断是否为隐藏项目
                 if (!(item[0] == "." && !settings.showHidden)) {
                     html += `<mdui-list-item rounded headline="` + item.replace("/", "") + `" type="` + (item.indexOf("/") != -1 ? "folder" : "file") + `" onclick="openItem('` + (path + item) + `');" class="file-list-item"></mdui-list-item>`;
                 }
@@ -243,6 +253,9 @@ function loadFiles(path) {
         setTimeout(function () {
             // 填入文件列表的 HTML 代码
             document.querySelector("#fileList").innerHTML = html;
+            if (document.querySelectorAll(".file-list-item").length == 0) {
+                document.querySelector("#fileList").innerHTML += `<div style="text-align: center;margin: 16px 0;" id="noFileListItem">没有项目</div>`;
+            }
             // 显示当前路径
             document.querySelector("#path").innerText = path;
             document.querySelector("#fileList").style.opacity = "1";
@@ -290,14 +303,15 @@ function loadFiles(path) {
         }, settings.showAnimation ? 250 : 0);
         localStorage.setItem("lastPath", path);
     } catch (error) {
-        // 捕获到异常时用 Snackbar 显示
-        document.querySelector("#error").innerText = error;
+        // 捕获到异常时用 Snackbar 提示
+        document.querySelector("#error").innerText = "未能载入项目列表：\n" + error;
         document.querySelector("#error").open = true;
+        document.querySelector("#loadingFiles").innerText = "没有项目";
     }
     // 检测 FAB 透明度设置
     document.querySelector("#FAB").style.opacity = settings.translucentFAB ? "0.8" : "1";
 }
-// 打开文件（夹）
+// 打开项目
 function openItem(item) {
     // 判断是否为文件夹
     if (item[item.length - 1] == "/") {
@@ -306,22 +320,30 @@ function openItem(item) {
         try {
             window.webapp.openfile(item);
         } catch (error) {
-            // 捕获到异常时用 Snackbar 显示
-            document.querySelector("#error").innerText = error;
+            // 捕获到异常时用 Snackbar 提示
+            document.querySelector("#error").innerText = "未能打开文件：\n" + error;
             document.querySelector("#error").open = true;
         }
     }
 }
-// 高亮文件（夹）
+// 高亮项目
 function highlightItem(target) {
+    // 记录成功状态
+    var success = false;
+    // 查找要高亮的项目
     document.querySelectorAll(".file-list-item").forEach(function (item) {
         if (item.headline == target) {
+            success = true;
             // 激活列表项
             item.active = "active";
             // 滚动到该项目
             document.querySelector("mdui-layout-main").scrollTo(0, item.offsetTop);
         }
     });
+    // 定位失败时用 Snackbar 提示
+    if (!success) {
+        document.querySelector("#failedToLocate").open = true;
+    }
 }
 // 搜索项目
 function searchItem() {
@@ -329,18 +351,23 @@ function searchItem() {
     var keyword = document.querySelector("#searchKeyword").value;
     // 判断是否区分大小写
     var matchCase = document.querySelector("#searchMatchCase").checked;
+    // 搜索结果计数
+    var searchResults = 0;
     if (keyword == "") {
         // 关键词为空时显示所有项目
         document.querySelectorAll(".file-list-item").forEach(function (item) {
             item.style.display = "block";
+            searchResults++;
         });
     } else {
+        // 查找项目
         if (matchCase) {
             document.querySelectorAll(".file-list-item").forEach(function (item) {
                 if (item.headline.indexOf(keyword) == -1) {
                     item.style.display = "none";
                 } else {
                     item.style.display = "block";
+                    searchResults++;
                 }
             });
         } else {
@@ -349,13 +376,27 @@ function searchItem() {
                     item.style.display = "none";
                 } else {
                     item.style.display = "block";
+                    searchResults++;
                 }
             });
         }
     }
+    // 搜索无结果提示
+    if (searchResults == 0) {
+        if (document.querySelectorAll("#noFileListItem").length != 0) {
+            document.querySelector("#noFileListItem").style.display = "block";
+        } else {
+            document.querySelector("#fileList").innerHTML += `<div style="text-align: center;margin: 16px 0;" id="noFileListItem">没有项目</div>`;
+        }
+    } else if (document.querySelectorAll("#noFileListItem").length != 0) {
+        document.querySelector("#noFileListItem").style.display = "none";
+    }
+    if (document.querySelectorAll("#loadingFiles").length != 0) {
+        document.querySelector("#loadingFiles").style.display = "none";
+    }
     document.querySelector('#searchItemDialog').open = false;
 }
-// 跳转到指定目录
+// 跳转到指定目录（并定位到项目）
 function setPath() {
     // 获取输入的路径
     var path = document.querySelector("#pathInput").value;
@@ -364,8 +405,23 @@ function setPath() {
         document.querySelector('#invalidPath').open = true;
         document.querySelector("#pathInput").focus();
     } else {
-        loadFiles(path);
         document.querySelector('#setPathDialog').open = false;
+        // 避免对话框关闭动画卡顿
+        setTimeout(function () {
+            // 判断是否为文件夹
+            if (path[path.length - 1] == "/") {
+                loadFiles(path);
+            } else {
+                loadFiles(path.slice(0, path.lastIndexOf("/") + 1));
+                // 避免错误高亮
+                if (window.webapp.listfile(path.slice(0, path.lastIndexOf("/") + 1)) != undefined) {
+                    // 文件列表载入完成后高亮文件
+                    setTimeout(function () {
+                        highlightItem(path.slice(path.lastIndexOf("/") + 1, path.length));
+                    }, 300);
+                }
+            }
+        }, 250);
     }
 }
 // 打开抽屉栏中的文件夹 / 定位到抽屉栏中的文件
@@ -412,11 +468,12 @@ icons = [
     { match: "crdownload", icon: "downloading" },
     { match: "themes,msstyles", icon: "style" },
     { match: "vcf", icon: "contacts" },
-    { match: "prop", icon: "info" }
+    { match: "prop", icon: "info" },
+    { match: "pem,der,cer,crt,key,csr,p12,0", icon: "vpn_key" }
 ];
 // 文件操作相关
 //
-// 新建文件（夹）
+// 新建项目
 function newItem() {
     // 获取项目类型和名称
     var type = document.querySelector("#newItemType").value;
@@ -439,31 +496,52 @@ function newItem() {
         // 存在同名项目时要求修改名称
         document.querySelector("#itemExists").open = true;
         document.querySelector("#newItemName").focus();
+    } else if (name.indexOf("/") != -1) {
+        // 名称存在非法字符时要求修改
+        document.querySelector("#illegalCharacter").open = true;
+        document.querySelector("#newItemName").focus();
     } else {
         if (type == "folder") {
             // 新建文件夹
             window.webapp.savefile(document.querySelector("#path").innerText + name, null);
-            // 自动进入该文件夹
-            openItem(document.querySelector("#path").innerText + name + "/");
+            // 检查新建是否成功
+            if (window.webapp.stayfile(document.querySelector("#path").innerText + name)) {
+                document.querySelector("#created").open = true;
+                // 避免对话框关闭动画卡顿
+                setTimeout(function () {
+                    // 自动进入该文件夹
+                    openItem(document.querySelector("#path").innerText + name + "/");
+                }, 250);
+            } else {
+                document.querySelector("#failedToCreate").open = true;
+            }
         } else {
             // 新建文件
             window.webapp.savefile(document.querySelector("#path").innerText + name, "");
-            refreshFileList();
-            setTimeout(function () {
-                // 高亮该文件
-                highlightItem(name);
-            }, 300);
+            // 检查新建是否成功
+            if (window.webapp.stayfile(document.querySelector("#path").innerText + name)) {
+                document.querySelector("#created").open = true;
+                // 避免对话框关闭动画卡顿
+                setTimeout(function () {
+                    refreshFileList();
+                    setTimeout(function () {
+                        // 高亮该文件
+                        highlightItem(name);
+                    }, 300);
+                }, 250);
+            } else {
+                document.querySelector("#failedToCreate").open = true;
+            }
         }
-        document.querySelector("#created").open = true;
         document.querySelector("#newItemDialog").open = false;
     }
 }
-// 复制文件（夹）完整路径
+// 复制项目完整路径
 function copyItemPath() {
     copyHandle(document.querySelector("#path").innerText + selected);
     document.querySelector("#itemMenuDialog").open = false;
 }
-// 重命名文件（夹）
+// 重命名项目
 function renameItem(isFinalStep = false) {
     // 获取新的名称
     var name = document.querySelector("#renameItemName").value;
@@ -484,15 +562,27 @@ function renameItem(isFinalStep = false) {
             // 存在同名项目时要求修改名称
             document.querySelector("#itemExists").open = true;
             document.querySelector("#renameItemName").focus();
+        } else if (name.indexOf("/") != -1) {
+            // 名称存在非法字符时要求修改
+            document.querySelector("#illegalCharacter").open = true;
+            document.querySelector("#renameItemName").focus();
         } else {
             window.webapp.namefile(document.querySelector("#path").innerText + selected, name);
-            document.querySelector("#renamed").open = true;
-            refreshFileList();
+            // 检查重命名是否成功
+            if (window.webapp.stayfile(document.querySelector("#path").innerText + name)) {
+                document.querySelector("#renamed").open = true;
+                // 避免对话框关闭动画卡顿
+                setTimeout(function () {
+                    refreshFileList();
+                    setTimeout(function () {
+                        // 高亮该文件
+                        highlightItem(name);
+                    }, 300);
+                }, 250);
+            } else {
+                document.querySelector("#failedToRename").open = true;
+            }
             document.querySelector("#renameItemDialog").open = false;
-            setTimeout(function () {
-                // 高亮该文件
-                highlightItem(name);
-            }, 300);
         }
     } else {
         document.querySelector("#itemMenuDialog").open = false;
@@ -502,13 +592,21 @@ function renameItem(isFinalStep = false) {
         document.querySelector("#renameItemName").value = selected;
     }
 }
-// 删除文件（夹）
+// 删除项目
 function deleteItem(isFinalStep = false) {
     if (isFinalStep) {
         // 最终操作
         window.webapp.awayfile(document.querySelector("#path").innerText + selected);
-        document.querySelector("#deleted").open = true;
-        refreshFileList();
+        // 检查删除是否成功
+        if (window.webapp.stayfile(document.querySelector("#path").innerText + selected)) {
+            document.querySelector("#failedToDelete").open = true;
+        } else {
+            document.querySelector("#deleted").open = true;
+            // 避免对话框关闭动画卡顿
+            setTimeout(function () {
+                refreshFileList();
+            }, 250);
+        }
         document.querySelector("#deleteItemDialog_2").open = false;
     } else {
         document.querySelector("#itemMenuDialog").open = false;
@@ -521,7 +619,7 @@ function deleteItem(isFinalStep = false) {
 // 书签相关
 //
 // 获取书签
-function loadBookmark() {
+function loadBookmarks() {
     if (localStorage.bookmark == undefined) {
         // 初始化书签列表
         bookmark = [];
@@ -556,12 +654,28 @@ function loadBookmark() {
     }
 }
 // 添加书签
-function addBookmark() {
-    bookmark.push({ icon: selectedIcon, headline: selected, description: document.querySelector("#path").innerText });
-    localStorage.setItem("bookmark", JSON.stringify(bookmark));
-    document.querySelector("#addedBookmark").open = true;
-    document.querySelector("#itemMenuDialog").open = false;
-    loadBookmark();
+function addBookmark(isCurrentPath = false) {
+    // 判断是否为当前目录添加书签
+    if (isCurrentPath) {
+        // 获取当前路径
+        var path = document.querySelector("#path").innerText;
+        // 判断路径是否为空
+        if (path == "") {
+            document.querySelector("#failedToGetPath").open = true;
+        } else {
+            path = path.slice(0, path.length - 1);
+            bookmark.push({ icon: "folder", headline: path.slice(path.lastIndexOf("/") + 1, path.length), description: path.slice(0, path.lastIndexOf("/") + 1) });
+            localStorage.setItem("bookmark", JSON.stringify(bookmark));
+            document.querySelector("#addedBookmark").open = true;
+            loadBookmarks();
+        }
+    } else {
+        bookmark.push({ icon: selectedIcon, headline: selected, description: document.querySelector("#path").innerText });
+        localStorage.setItem("bookmark", JSON.stringify(bookmark));
+        document.querySelector("#addedBookmark").open = true;
+        document.querySelector("#itemMenuDialog").open = false;
+        loadBookmarks();
+    }
 }
 // 移除书签
 function removeBookmark() {
@@ -569,7 +683,7 @@ function removeBookmark() {
     localStorage.setItem("bookmark", JSON.stringify(bookmark));
     document.querySelector("#removedBookmark").open = true;
     document.querySelector("#deleteBookmarkDialog").open = false;
-    loadBookmark();
+    loadBookmarks();
 }
 // 批量操作相关
 //
@@ -621,10 +735,27 @@ function multipleOperate(option, isFinalStep = false) {
                     multipleSelected.forEach(function (item) {
                         window.webapp.awayfile(document.querySelector("#path").innerText + item);
                     });
-                    document.querySelector('#multipleDeleteDialog_2').open = false;
-                    document.querySelector("#multipleOperationStatus").innerText = "已删除" + multipleSelected.length + "个项目";
+                    // 删除失败计数
+                    var failed = 0;
+                    multipleSelected.forEach(function (item) {
+                        if (window.webapp.stayfile(document.querySelector("#path").innerText + item)) {
+                            failed++;
+                        }
+                    });
+                    if (failed == 0) {
+                        var snackbarText = "已删除" + (multipleSelected.length - failed) + "个项目";
+                    } else if (failed == multipleSelected.length) {
+                        var snackbarText = "未能删除" + failed + "个项目";
+                    } else {
+                        var snackbarText = "已删除" + (multipleSelected.length - failed) + "个项目，未能删除" + failed + "个项目";
+                    }
+                    document.querySelector("#multipleOperationStatus").innerText = snackbarText;
                     document.querySelector("#multipleOperationStatus").open = true;
-                    refreshFileList();
+                    document.querySelector('#multipleDeleteDialog_2').open = false;
+                    // 避免对话框关闭动画卡顿
+                    setTimeout(function () {
+                        refreshFileList();
+                    }, 250);
                 } else {
                     document.querySelector('#multipleSelectDialog').open = false;
                     document.querySelector('#multipleDeleteDialog').open = true;
